a6b4f6611a00416db428aedfc0de4c311b26ae96
jesse seavers <jseav19@gmail.com>
2011-05-16 18:23:33 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aqalitylife::Application.config.secret_token = '221f67060bf0073292a71fb082c63feb65a1a94b68eb29be70bdbfb36ba4dbbda166bcb9104e279e8b0f7410aee36ca9f0e7436908060b13814c687f21ff859d'
